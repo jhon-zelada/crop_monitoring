6a36185e00a942667c602a3271b8005c63c6c8e9
jhon-zelada <jhonzelada01@gmail.com>
2025-09-18 07:59:17 -0500

    modiffying the db

--- backend/app/scripts/init_db.py
+++ backend/app/scripts/init_db.py
@@ -6,6 +6,7 @@ db = SessionLocal()
 
 device = models.Device(
     id=uuid.UUID("11111111-1111-1111-1111-111111111111"),
+    message_id="msg-0001",
     name="Parcela 1 - Quinua",
     device_type="sensor-node",
     token="supersecrettoken123",
